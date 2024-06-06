ce017d935932a017a6e893edd334c55c720ca064
DarkBear0 <baphometmarx@gmail.com>
2024-06-06 09:08:03 +0200

    Price correction

--- src/TicketService.java
+++ src/TicketService.java
@@ -9,7 +9,7 @@ public class TicketService {
         tickets = new HashMap<>();
         for (int i = 1; i <= 10; i++) {
             String id = String.format("%04d", i);
-            Ticket ticket = new Ticket(id, "Hall" + (i % 3 + 1), 100 + i, System.currentTimeMillis(), false, (char)('A' + i % 3), 5.0, new TicketPrice(100.0, 10.0));
+            Ticket ticket = new Ticket(id, "Hall" + (i % 3 + 1), 100 + i, System.currentTimeMillis(), false, (char)('A' + i % 3), 5.0, new TicketPrice(100, 10));
             tickets.put(id, ticket);
         }
     }
