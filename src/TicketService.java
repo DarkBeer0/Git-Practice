2ccc952da9381ec19a26fbfce976a9eff7ea4fc9
DarkBear0 <baphometmarx@gmail.com>
2024-06-06 08:48:20 +0200

    project migration

--- src/TicketService.java
+++ src/TicketService.java
@@ -11,5 +11,6 @@ public class TicketService {
 
         Ticket limitedTicket = new Ticket("SmallHall", 456, Instant.now().getEpochSecond());
         System.out.println("Limited Ticket: " + limitedTicket);
+        //test
     }
 }
